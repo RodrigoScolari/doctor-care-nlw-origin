9e4f6f237729fa3aa941edb2f885cc6327b2fd57
Rodrigo A Scolari <rodrigosar06@gmail.com>
2022-05-17 20:19:45 -0300

    style: add underline stile at navbar when taht section is active

--- main.js
+++ main.js
@@ -4,6 +4,32 @@ onScroll()
 function onScroll() {
   showNavOnScroll()
   showBackToTopBtnOnScroll()
+
+  activateMenuAtCurrentSection(home)
+  activateMenuAtCurrentSection(services)
+  activateMenuAtCurrentSection(about)
+  activateMenuAtCurrentSection(contact)
+}
+
+function activateMenuAtCurrentSection(section) {
+  const targetLine = scrollY + innerHeight /2
+
+  const sectionTop = section.offsetTop 
+  const sectionHeight = section.offsetHeight 
+  const sectionTopReachOrPassTargetLine = targetLine >= sectionTop
+
+  const sectionEndsAt = sectionTop + sectionHeight 
+  const sectionEndPassedTargetLine = sectionEndsAt <= targetLine
+  
+  const sectionBoundaries = sectionTopReachOrPassTargetLine && !sectionEndPassedTargetLine
+
+  const sectionID = section.getAttribute('id')
+  const menuElement = document.querySelector(`.menu a[href*=${sectionID}]`)
+
+  menuElement.classList.remove('active')
+  if (sectionBoundaries) {
+    menuElement.classList.add('active')
+  }
 }
 
 function showNavOnScroll() {
